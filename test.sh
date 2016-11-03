a8e91eb2a7f478ea9d6c2dc8f5399b5443b4913f
root <root@test0.reviakin.net>
2016-11-03 17:04:11 +0500

    some fixes

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./service.php verbose debug:4 srvaddr:192.168.68.1 srvport:5038 srvuser:astami srvpass:astamipassword112
+./service.php verbose debug:4 srvaddr:192.168.68.1 srvport:5038 srvuser:astami srvpass:astamipassword112 ocisrv: ocisvc:polus ociuser:sys ocipass:Polus_sysPwd
