03ab296d890080fa42074f406086cbffaba6c5bc
root@asterisk.troitsk.nppx.local <admin@nppx.ru>
2017-11-14 23:29:42 +0500

    removed garbage

--- test.bat
+++
@@ -1,4 +0,0 @@
-::php "service.php" verbose debug:4 srvaddr:79.134.17.146 srvport:3850 srvuser:spookieast srvpass:spookieast wsaddr:192.168.0.3 wsport:18080 wschan:testchan3
-::php "service.php" verbose debug:4 srvaddr:ns.ural.keramir.com srvport:3850 srvuser:zabbix srvpass:zabbixpassword112
-php svc.watch_ami.php verbose debug:3 srvaddr:192.168.170.30 srvport:5038 srvuser:astami srvpass:AstAm1pa55word112 conout:yes oci1srvv:srv-db.nppx.local ociinst:orcl ociuser:ics ocipass:Trk_icsPwd
-::wsaddr:192.168.0.3 wsport:18080 wschan:testchan3
