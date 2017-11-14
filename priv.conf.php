a9e75bf4e6c4ab0c2663f169550b82c4d99b757d
root@asterisk.troitsk.nppx.local <admin@nppx.ru>
2017-11-14 23:28:45 +0500

    removed garbage

--- priv.conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$services_list=[
-		'svc.watch_ami.php'=>'srvaddr:127.0.0.1 srvport:5038 srvuser:astami srvpass:AstAm1pa55word112 ocisrv:192.168.170.10 ociinst:orcl ociuser:ics ocipass:Trk_icsPwd',
-	]
-?>
